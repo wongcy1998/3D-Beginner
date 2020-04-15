b44b57d75aec3a3680738649ea73f31906a1c943
Jake Gianola <jakegianola204@gmail.com>
2020-04-15 14:52:39 -0700

    Ending the Game done

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeduration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    bool m_IsPlayerAtExit;
+    public CanvasGroup ExitBackgroundImageCanvasGroup;
+    float m_Timer;
+
+    void OnTriggerEnter (Collider other){
+        if (other.gameObject == player) {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void Update() {
+        if (m_IsPlayerAtExit){
+            EndLevel();
+        }
+    }
+
+    void EndLevel() {
+        m_Timer += Time.deltaTime;
+        ExitBackgroundImageCanvasGroup.alpha = m_Timer / fadeduration;
+
+        if(m_Timer > fadeduration + displayImageDuration){
+            Application.Quit();
+        }
+    }
+}
